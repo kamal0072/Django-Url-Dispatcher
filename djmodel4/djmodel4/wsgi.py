39e6ceb34af56604706e74b3356816cb26d109bb
kamal hasan <hasan.kamaal0072@gmail.com>
2021-03-17 18:11:18 -0800

    Django Form WIth Validation

--- djmodel4/djmodel4/wsgi.py
+++ djmodel4/djmodel4/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djmodel3 project.
+WSGI config for djmodel4 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djmodel3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djmodel4.settings')
 
 application = get_wsgi_application()
---
+++ djmodel4/myform/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+class EmployeForm(forms.Form):
+    e_name=forms.CharField(label='Employe Name:',label_suffix='-',
+    help_text='Enter Emplyoye Name',initial="david")
+
+    e_email=forms.EmailField(label='Employe Email:',initial='abc@gmail.com',
+    help_text='Enter Employe Email',widget=forms.HiddenInput())
+
+    # e_address=forms.CharField(initial='Enter Full Address',widget=forms.CheckboxInput(),
+    # e_address=forms.CharField(initial='Enter Full Address',widget=forms.RadioSelect(),
+    e_address=forms.CharField(initial='Enter Full Address',
+    widget=forms.Textarea(),
+    help_text='Enter Your Full Address')
+
+    e_pass=forms.CharField(label='Employe Password',required=True,
+    help_text='Enter maximum 70 char..',widget=forms.PasswordInput())
+    
+
